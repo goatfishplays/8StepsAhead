b5afface8566aecd66b15560c9b3a9b237d0e80f
goatfishplays <goatfishplays@gmail.com>
2024-10-04 18:05:12 -0700

    hats

--- Assets/InputsThing.cs
+++ Assets/InputsThing.cs
@@ -46,7 +46,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
                     ""initialStateCheck"": true
                 },
                 {
-                    ""name"": ""Fire"",
+                    ""name"": ""Use"",
                     ""type"": ""Button"",
                     ""id"": ""6883e900-0e06-4d66-bd25-c07ce204c392"",
                     ""expectedControlType"": ""Button"",
@@ -228,7 +228,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Gamepad"",
-                    ""action"": ""Fire"",
+                    ""action"": ""Use"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
@@ -239,7 +239,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Fire"",
+                    ""action"": ""Use"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
@@ -250,7 +250,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Touch"",
-                    ""action"": ""Fire"",
+                    ""action"": ""Use"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
@@ -261,7 +261,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Joystick"",
-                    ""action"": ""Fire"",
+                    ""action"": ""Use"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
@@ -272,7 +272,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""XR"",
-                    ""action"": ""Fire"",
+                    ""action"": ""Use"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -862,7 +862,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
-        m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Use = m_Player.FindAction("Use", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -938,14 +938,14 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
-    private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Use;
     public struct PlayerActions
     {
         private @InputsThing m_Wrapper;
         public PlayerActions(@InputsThing wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Look => m_Wrapper.m_Player_Look;
-        public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Use => m_Wrapper.m_Player_Use;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -961,9 +961,9 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
             @Look.started += instance.OnLook;
             @Look.performed += instance.OnLook;
             @Look.canceled += instance.OnLook;
-            @Fire.started += instance.OnFire;
-            @Fire.performed += instance.OnFire;
-            @Fire.canceled += instance.OnFire;
+            @Use.started += instance.OnUse;
+            @Use.performed += instance.OnUse;
+            @Use.canceled += instance.OnUse;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -974,9 +974,9 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
             @Look.started -= instance.OnLook;
             @Look.performed -= instance.OnLook;
             @Look.canceled -= instance.OnLook;
-            @Fire.started -= instance.OnFire;
-            @Fire.performed -= instance.OnFire;
-            @Fire.canceled -= instance.OnFire;
+            @Use.started -= instance.OnUse;
+            @Use.performed -= instance.OnUse;
+            @Use.canceled -= instance.OnUse;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -1161,7 +1161,7 @@ public partial class @InputsThing: IInputActionCollection2, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
-        void OnFire(InputAction.CallbackContext context);
+        void OnUse(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,8 +12,19 @@ using UnityEngine.SceneManagement;
 public class Player : Entity
 {
     [Header("Movement+")]
-    public float sprintMult = 2f;
-    public bool isSprinting = false;
+
+    [Header("Friction")]
+    public float frictionAmount = 0.2f;
+
+
+    [Header("Ground Check")]
+    public Transform groundCheckPoint;
+    public Vector2 groundCheckSize = new Vector2(1, 1);
+    public LayerMask groundLayer;
+    public float groundedTimeBuffer = 0f;
+    public float lastGroundedTime = 0f;
+
+
 
     [Header("Hunger")]
     public bool changingHunger = true;
@@ -22,59 +33,20 @@ public class Player : Entity
     public float hungerChangeRate = -1f;
     public float hungerToHealth = 1f;
 
-    [Header("Temperature")]
-    public bool ChangingTemperature = true;
-    public float temperature = 100f;
-    public float maxTemperature = 100f;
-    public float temperatureChangeRate = 1f;
-
-    [Header("Anger")]
-    public bool ChangingAnger = true;
-    public float anger = 0;
-    public float maxAnger = 100f;
-    public float angerChangeRate = -.5f;
-
-    [Header("Sadness")]
-    public bool ChangingSadness = true;
-    public float sadness = 0;
-    public float maxSadness = 100f;
-    public float sadnessChangeRate = -.5f;
-
-    [Header("Fear")]
-    public bool ChangingFear = true;
-    public float fear = 0;
-    public float maxFear = 100f;
-    public float fearChangeRate = -2f;
-
     [Header("Bars")]
     public RectTransform HealthBar;
     public RectTransform HungerBar;
-    public RectTransform TemperatureBar;
-    public RectTransform AngerBar;
-    public RectTransform SadnessBar;
-    public RectTransform FearBar;
 
-    [Header("Inventory")]
-    public RectTransform SelectedItemKarat;
-    public TMP_Text SelectedItemCount;
-    public int selectedItem = 0;
-    public float dropWaitTime = 1f;
-    public Image[] ItemsHotbar;
-    public Sprite emptyItem;
-    public Item[] Inventory = { null, null, null, null, null, null };
     public bool useInputCached = false;
 
 
     [Header("Other Stuff")]
 
-    // Rigidbody2D rb;
-    // Vector2 inputs = new Vector2(0, 0);
-    public EmotionWeatherManager emoWeather;
     public Transform holdPoint;
-    PlayerInputActionsThing pInputs;
+    InputsThing pInputs;
 
-    [SerializeField]
-    // private Camera playerCamera;
+    // [SerializeField]
+    // // private Camera playerCamera;
 
     protected override void Awake()
     {
@@ -82,104 +54,79 @@ public class Player : Entity
 
         // replace later if we figure out a different way to do enemies
         EnemyBasicAI.playerTransform = this.transform;
-        // Inventory = { null, null};
 
 
-
-        // rb = GetComponent<Rigidbody2D>();
-        // playerCamera = Camera.main;
-        // playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y, playerCamera.transform.position.z);
-        // playerCamera.transform.SetParent(transform);
-        pInputs = new PlayerInputActionsThing();
+        pInputs = new InputsThing();
         pInputs.Player.Enable();
-        pInputs.Player.Sprint.performed += ToggleSprint;
-        pInputs.Player.Scroll.performed += Scroll;
         pInputs.Player.Use.started += UseS;
         pInputs.Player.Use.canceled += UseE;
-        pInputs.Player.Drop.performed += Drop;
     }
 
     private void OnDestroy()
     {
-
-        pInputs.Player.Sprint.performed -= ToggleSprint;
-        pInputs.Player.Scroll.performed -= Scroll;
         pInputs.Player.Use.started -= UseS;
         pInputs.Player.Use.canceled -= UseE;
-        pInputs.Player.Drop.performed -= Drop;
     }
 
     protected override void Update()
     {
         base.Update();
-        // Gather Input
+        Vector2 moveInputs = pInputs.Player.Move.ReadValue<Vector2>();
 
-        // inputs = pInputs.Player.Move.ReadValue<Vector2>();
-        // print((Vector2)Camera.main.ScreenToWorldPoint(pInputs.Player.Look.ReadValue<Vector2>()));
-
-
-        // Use Item
-
+        // Use key
+        // -------
         if (useInputCached)
         {
-            if (Inventory[selectedItem] != null)
-            {
-                Inventory[selectedItem].Use((Vector2)Camera.main.ScreenToWorldPoint(pInputs.Player.Look.ReadValue<Vector2>()));
-                if (Inventory[selectedItem].count == 0)
-                {
-                    EmptySlot(selectedItem);
-                }
-                else
-                {
-                    SelectedItemCount.text = Inventory[selectedItem].count.ToString();
-                }
-                // print(Inventory[selectedItem].gameObject);
-            }
+
         }
 
 
-
         // Movement
-
+        // --------
         if (canMove)
         {
-            // calc moveSpeedMult
-            moveSpeedMult = 1f;
-            moveSpeedMult = moveSpeedMult * .7f + .3f * temperature / maxTemperature;
-            if (isSprinting)
-            {
-                moveSpeedMult *= sprintMult;
-            }
+            #region MovementH
 
             // calculate dir want to move and desired velo
-            Vector2 targetSpeed = pInputs.Player.Move.ReadValue<Vector2>() * moveSpeed * moveSpeedMult;
+            float targetSpeed = moveInputs.x * moveSpeed * moveSpeedMult;
             // change accell depending on situation(if our target target speed wants to not be 0 use decell)
-            float accelRate = targetSpeed.magnitude > .01f ? accel : deccel;
+            float accelRate = targetSpeed > .01f ? accel : deccel;
             // calc diff between current and target
-            Vector2 speedDif = targetSpeed - rb.velocity;
+            float speedDif = targetSpeed - rb.velocity.x;
             // applies accel to speed diff, raises to power so accel will increase with higher speeds then applies to desired dir
-            Vector2 movement = Mathf.Pow(speedDif.magnitude * accelRate, velPower) * speedDif.normalized;
+            float movement = Mathf.Pow(speedDif * accelRate, velPower) * Mathf.Sign(speedDif);
             // apply force
-            rb.AddForce(movement);
+            rb.AddForce(Vector2.right * movement);
+
+            #endregion
+
+            #region Friction
+            // if grounded and trying to stop apply extra friction
+            if (lastGroundedTime > 0 && Mathf.Abs(moveInputs.x) < 0.01f)
+            {
+                // use friction ammount
+                float amt = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
+                // sets to movement dir
+                amt *= Mathf.Sign(rb.velocity.x);
+                // apply force against movement dir
+                rb.AddForce(Vector2.right * -frictionAmount, ForceMode2D.Impulse);
+            }
+            #endregion
+
+            #region groundCheck
+            if (Physics2D.OverlapBox(groundCheckPoint.position, groundCheckSize, 0, groundLayer))
+            {
+                lastGroundedTime = groundedTimeBuffer;
+            }
+            #endregion
+
 
         }
 
 
 
         // Update Stats
-
-        // Stats based on Stats
-
-        // fear -> fear
-        if (health / maxHealth > .7f) // passive decrease above 70 health
-        {
-            ChangeFear(Time.deltaTime * fearChangeRate);
-        }
-        else if (health / maxHealth < .5f) // passive increase below 50 health based on health
-        {
-            ChangeFear(Time.deltaTime * -fearChangeRate / (health / maxHealth));
-        }
-
+        #region StatsUpdatingStuff
         // hunger -> health
         ChangeHunger(Time.deltaTime * hungerChangeRate);
         if (hunger > 50) // pasive health increase 
@@ -190,37 +137,14 @@ public class Player : Entity
         {
             ChangeHealth(-hungerToHealth * Time.deltaTime, false);
         }
-
-        // if sad(it will be raining) and afraid(will be windy) change temp accordingly
-        ChangeTemperature(Time.deltaTime * -1.5f * (sadness / maxSadness + fear / maxFear));
-        ChangeTemperature(Time.deltaTime * temperatureChangeRate);
-        ChangeAnger(Time.deltaTime * angerChangeRate);
-        ChangeSadness(Time.deltaTime * sadnessChangeRate);
+        #endregion
 
     }
 
-    // private void Use(InputAction.CallbackContext context)
-    // {
-    //     if (Inventory[selectedItem] != null)
-    //     {
-    //         Inventory[selectedItem].Use((Vector2)Camera.main.ScreenToWorldPoint(pInputs.Player.Look.ReadValue<Vector2>()));
-    //     }
-    // }
-    private void EmptySlot(int i)
+    private void Jump()
     {
-        ItemsHotbar[i].sprite = emptyItem;
-        Inventory[i] = null;
-        SelectedItemCount.text = "0";
-    }
-    private void Drop(InputAction.CallbackContext context)
-    {
-        if (Inventory[selectedItem] != null)
-        {
-            Inventory[selectedItem].transform.SetParent(null);
-            Inventory[selectedItem].owner = null;
-            Inventory[selectedItem].dropWaitTime = dropWaitTime;
-            EmptySlot(selectedItem);
-        }
+        // apply force
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void UseS(InputAction.CallbackContext context)
@@ -233,104 +157,8 @@ public class Player : Entity
         useInputCached = false;
     }
 
-
-
-    private void Scroll(InputAction.CallbackContext context)
-    {
-        // print(context.ReadValue<float>());
-        if (Inventory[selectedItem] != null)
-        {
-            Inventory[selectedItem].GetComponent<SpriteRenderer>().enabled = false;
-        }
-        if (context.ReadValue<float>() > 0)
-        {
-            selectedItem++;
-            if (selectedItem >= ItemsHotbar.Length)
-            {
-                selectedItem = 0;
-            }
-        }
-        else
-        {
-            selectedItem--;
-            if (selectedItem < 0)
-            {
-                selectedItem = ItemsHotbar.Length - 1;
-            }
-
-        }
-        SelectedItemKarat.localPosition = new Vector2(-42 + 17 * selectedItem, 12);
-        if (Inventory[selectedItem] != null)
-        {
-            Inventory[selectedItem].GetComponent<SpriteRenderer>().enabled = true;
-            SelectedItemCount.text = Inventory[selectedItem].count.ToString();
-        }
-        else
-        {
-            SelectedItemCount.text = "0";
-        }
-    }
-
-    public void AttemptPickup(Item item)
-    {
-        int placeLoc = -1;
-        for (int i = 0; i < Inventory.Length; i++)
-        {
-            if (Inventory[i] == null)
-            {
-                if (placeLoc == -1)
-                {
-                    placeLoc = i;
-                }
-            }
-            else if (Inventory[i].itemId == item.itemId && item.stacks && item.maxStack > Inventory[i].count)
-            {
-                int temp = Mathf.Min(item.maxStack - Inventory[i].count, item.count);
-                Inventory[i].count += temp;
-                item.ChangeCount(-temp);
-                if (selectedItem == i)
-                {
-                    SelectedItemCount.text = Inventory[selectedItem].count.ToString();
-                }
-                // Destroy(item.gameObject);
-                return;
-            }
-        }
-        if (placeLoc != -1)
-        {
-            Inventory[placeLoc] = item;
-            item.owner = gameObject;
-            ItemsHotbar[placeLoc].sprite = item.GetComponent<SpriteRenderer>().sprite;
-            item.transform.parent = transform;
-            item.transform.position = holdPoint.position;
-            if (placeLoc != selectedItem)
-            {
-                item.GetComponent<SpriteRenderer>().enabled = false;
-            }
-            else
-            {
-                SelectedItemCount.text = Inventory[selectedItem].count.ToString();
-            }
-        }
-    }
-
-    private void ToggleSprint(InputAction.CallbackContext context)
-    {
-        isSprinting = !isSprinting;
-    }
-
     public override void ChangeHealth(float value, bool addsIframes = true)
     {
-        if (value < -5.5f && iFrameTime <= 0)
-        {
-            // anger -= value;
-            ChangeAnger(-.5f * value);
-            // fear -= value * .7f;
-            ChangeFear(-.3f * value);
-            // sadness -= value * .5f;
-            ChangeSadness(-.1f * value);
-            emoWeather.lightningTime = 0;
-        }
         base.ChangeHealth(value, addsIframes);
         HealthBar.localScale = new Vector3(1, 1 - health / maxHealth, 1);
     }
@@ -350,79 +178,6 @@ public class Player : Entity
             }
         }
         HungerBar.localScale = new Vector3(1, 1 - hunger / maxHunger, 1);
-    }
-    public void ChangeTemperature(float value)
-    {
-        if (ChangingTemperature)
-        {
-            temperature += value;
-            if (temperature > maxTemperature)
-            {
-                temperature = maxTemperature;
-            }
-            else if (temperature < 0)
-            {
-                temperature = 0;
-            }
-        }
-        TemperatureBar.localScale = new Vector3(1, 1 - temperature / maxTemperature, 1);
-    }
-    public void ChangeAnger(float value)
-    {
-        if (ChangingAnger)
-        {
-            if (value < 0 && hunger < 25) // if hungry multiply emotion values
-            {
-                anger *= 1.5f;
-            }
-            anger += value;
-            if (anger > maxAnger)
-            {
-                anger = maxAnger;
-            }
-            else if (anger < 0)
-            {
-                anger = 0;
-            }
-        }
-        AngerBar.localScale = new Vector3(1, 1 - anger / maxAnger, 1);
-    }
-    public void ChangeSadness(float value)
-    {
-        if (hunger > 50 && value > 0) // if hungry multiply emotion values
-        {
-            value *= 1.5f;
-        }
-        if (ChangingSadness)
-        {
-            sadness += value;
-            if (sadness > maxSadness)
-            {
-                sadness = maxSadness;
-            }
-            else if (sadness < 0)
-            {
-                sadness = 0;
-            }
-        }
-        SadnessBar.localScale = new Vector3(1, 1 - sadness / maxSadness, 1);
-        emoWeather.ResetSadboiTime();
-    }
-    public void ChangeFear(float value)
-    {
-        if (ChangingFear)
-        {
-            fear += value;
-            if (fear > maxFear)
-            {
-                fear = maxFear;
-            }
-            else if (fear < 0)
-            {
-                fear = 0;
-            }
-        }
-        FearBar.localScale = new Vector3(1, 1 - fear / maxFear, 1);
     }
 
     public override void Die()
